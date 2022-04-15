83228eb3e0ddb2e1e8ba7dd6414f82820356a2d4
Sarv3sh12 <77911731+Sarv3sh12@users.noreply.github.com>
2022-04-15 17:59:54 +0530

    Add files via upload

---
+++ Food Ordering Website/onClickMenu.js
@@ -0,0 +1,10 @@
+function onClickMenu(){
+    document.getElementById('menu-bar').classList.toggle('change');
+    document.getElementById('nav').classList.toggle('change-btn');}
+    function openNav() {
+        document.getElementById("myNav").style.width = "25%";
+      }
+      
+      function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+      }
